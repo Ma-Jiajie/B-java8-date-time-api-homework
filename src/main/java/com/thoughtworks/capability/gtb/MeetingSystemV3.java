7ed3df69e41453955369579450654bd9e8eb8999
jiajie <jiajie.ma@thoughtworks.com>
2020-10-24 22:37:08 +0800

    [Jiajie]: MeetingSystemV3 is finished.

--- src/main/java/com/thoughtworks/capability/gtb/MeetingSystemV3.java
+++ src/main/java/com/thoughtworks/capability/gtb/MeetingSystemV3.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -23,16 +25,28 @@ public class MeetingSystemV3 {
     // 根据格式创建格式化类
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     // 从字符串解析得到会议时间
-    LocalDateTime meetingTime = LocalDateTime.parse(timeStr, formatter);
+    LocalDateTime meetingTime = LocalDateTime.parse(timeStr, formatter); //伦敦会议时间
+    ZoneId zoneIdLondon = ZoneId.of("Europe/London");  //添加伦敦时区
+    ZonedDateTime zonedDateTimeLondon = ZonedDateTime.of(meetingTime, zoneIdLondon); //设置伦敦时区时间
+
+    ZoneId zoneIddefault = ZoneId.systemDefault(); //获取当前系统时区
+    LocalDateTime localDateTimeforMeeting = zonedDateTimeLondon.withZoneSameInstant(zoneIddefault).toLocalDateTime();  //本地时区会议时间：使用系统
 
     LocalDateTime now = LocalDateTime.now();
-    if (now.isAfter(meetingTime)) {
+    if (now.isAfter(localDateTimeforMeeting)) {
+
       LocalDateTime tomorrow = now.plusDays(1);
       int newDayOfYear = tomorrow.getDayOfYear();
-      meetingTime = meetingTime.withDayOfYear(newDayOfYear);
 
+      localDateTimeforMeeting = localDateTimeforMeeting.withDayOfYear(newDayOfYear);  //本地下次会议时间
+      ZonedDateTime zonedDateTimeDefault = localDateTimeforMeeting.atZone(zoneIddefault); // 设置新会议+当前时区
+
+      ZoneId zoneIdChicago = ZoneId.of("America/Chicago"); //添加芝加哥时区
+      ZonedDateTime zonedDateTimeChicago = zonedDateTimeDefault.withZoneSameInstant(zoneIdChicago);  //新会议转换芝加哥时区
+
+      LocalDateTime meetingTimeforChicago = zonedDateTimeChicago.toLocalDateTime(); //芝加哥会议时间
       // 格式化新会议时间
-      String showTimeStr = formatter.format(meetingTime);
+      String showTimeStr = formatter.format(meetingTimeforChicago);
       System.out.println(showTimeStr);
     } else {
       System.out.println("会议还没开始呢");
